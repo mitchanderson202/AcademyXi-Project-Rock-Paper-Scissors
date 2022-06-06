90ba3144bb5887bccfa7fb17771db83d2afd53fd
mitch <mitchellanderson202@hotmail.com>
2022-06-06 11:26:34 +1000

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [userChoice, setUserChoice] = useState();
+  const [randomOption, setRandomOption] = useState();
+  const choicesArray = ["Rock", "Paper", "Scissors"];
+
+  const computerPick = () => {
+    let random = choicesArray[Math.floor(Math.random()*choicesArray.length)];
+    setRandomOption(random);
+  }
+
+  const userClick = (value) => {
+    setUserChoice(value);
+    computerPick();    
+  }
+
+  const reset = () => {
+    setUserChoice(null);
+    setRandomOption(null);
+  }      
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Pick Rock, Paper or Scissors. This will generate a random pick for the computer. Press Reset to start again. Good luck.</p>
+      <h1>Choose your weapon...</h1>
+      <button onClick={() => userClick("Rock")}>Rock</button>
+      <button onClick={() => userClick("Paper")}>Paper</button>
+      <button onClick={() => userClick("Scissors")}>Scissors</button>
+      <h3>You chose: {userChoice}</h3>
+      <h3>Computer chose: {randomOption}</h3>
+      <h4> Result: {
+        userChoice === randomOption ? "Tie" :
+        userChoice === "Rock" && randomOption === "Scissors" ? "Winner" :
+        userChoice === "Paper" && randomOption === "Rock" ? "Winner" :
+        userChoice === "Scissors" && randomOption === "Paper" ? "Winner" :
+        "Loser"
+        } </h4>        
+      <button onClick={() => reset()}>Reset</button>
     </div>
-  );
-}
+  )
+
+  }
+  
 
 export default App;
